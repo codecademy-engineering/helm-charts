970382fddda9d41f40213eb5ec4bdf5ad5807861
Scott Rigby <scott@r6by.com>
2019-05-03 03:20:25 -0400

    Update release script
    Signed-off-by: Scott Rigby <scott@r6by.com>

--- .circleci/release.sh
+++ .circleci/release.sh
@@ -7,24 +7,40 @@ set -o pipefail
 : "${GIT_USERNAME:?Environment variable GIT_USERNAME must be set}"
 : "${GIT_EMAIL:?Environment variable GIT_EMAIL must be set}"
 
-CHANGED_CHARTS="$(git diff --find-renames --name-only origin/master -- charts | cut -d '/' -f 2 | uniq)"
+# Always generate new index.yaml file from charts in master, for comparison
+# with existing file in gh-pages branch.
 
-if [ -n "$CHANGED_CHARTS" ]; then
-    rm -rf .deploy
-    mkdir -p .deploy
+# Ensure local copy is not out of date because of circle jobs overrunning one
+# another out of order.
+git fetch origin
+git pull origin master
 
-    for CHART in $CHANGED_CHARTS; do
-        helm dependency build charts/$CHART
-        helm package charts/${CHART} --destination .deploy
-    done
+# Clean directory to add packaged charts, and index.yaml.
+rm -rf .deploy
+mkdir -p .deploy
+for CHART in charts/*; do
+    helm dependency build $CHART
+    helm package $CHART --destination .deploy
+done
+helm repo index .deploy --url https://${CIRCLE_PROJECT_USERNAME}.github.io/${CIRCLE_PROJECT_REPONAME}
 
-    helm repo index .deploy --url https://${CIRCLE_PROJECT_USERNAME}.github.io/${CIRCLE_PROJECT_REPONAME}
+# Get newly generaged index.yaml values for comparison. Note timestamps will
+# never match, so remove them for comparison.
+NEW_INDEX=`yq r .deploy/index.yaml -j | jq 'del(.. | .created?)' | jq 'del(.. | .generated?)'`
 
-    git checkout gh-pages
+# Check out helm repo gh-pages branch, to compare existing index.yaml.
+git checkout gh-pages
+OLD_INDEX=`yq r index.yaml -j | jq 'del(.. | .created?)' | jq 'del(.. | .generated?)'`
+
+if [ "$NEW_INDEX" != "$OLD_INDEX" ]; then
+    # If the index files (without dates) are not identical, remove any existing
+    # artifacts, and copy the new one in place.
+    git rm .
     cp --force .deploy/* .
     rm -r .deploy
     git add .
 
+    # Commit and push changes to gh-pages branch.
     git config user.email "$GIT_EMAIL"
     git config user.name "$GIT_USERNAME"
     git commit --message "Update Helm repo index and packages" --signoff
